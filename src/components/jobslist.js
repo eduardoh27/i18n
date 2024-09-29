b5f57c8d1a74867ca6b46dc7da0c4604580818cb
Eduardo Herrera <78046940+eduardoh27@users.noreply.github.com>
2024-09-29 00:16:25 -0500

    done

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Job from "./job";
+import {FormattedMessage} from 'react-intl';
 
 const JobsList = () => {
   const [offers] = useState([
@@ -35,7 +36,9 @@ const JobsList = () => {
         <thead className="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
+            <th scope="col">
+                <FormattedMessage id="Position"/>
+            </th>
             <th scope="col">Company</th>
             <th scope="col">Salary</th>
             <th scope="col">City</th>
